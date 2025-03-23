d7276123d2d76e4440d248c496314b8c411947cb
NightOwlEyes <120246177+NightOwlEyes@users.noreply.github.com>
2025-03-24 04:10:26 +0700

    Add files via upload

---
+++ nightowleyes/app/views/errors/404.php
@@ -0,0 +1,12 @@
+<?php include 'app/views/shares/header.php'; ?>
+
+<div class="error-page" style="height: 100vh; width: 100vw; overflow: hidden; position: relative;">
+    <div class="error-image" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
+        <img src="/nightowleyes/uploads/404/404.png" alt="404 Error" style="width: 100%; height: 100%; object-fit: cover;">
+    </div>
+    <div style="position: absolute; bottom: 20px; left: 50%; transform: translateX(-50%); z-index: 10;">
+        <a href="/nightowleyes/product" class="btn btn-primary btn-lg">
+            <i class="fas fa-home"></i> Về trang chủ
+        </a>
+    </div>
+</div>
